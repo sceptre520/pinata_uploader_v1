455eb4b78d813400dfa10dc6573c2f3ca66644ec
sceptre520 <sceptre520@gmail.com>
2022-02-05 16:38:06 +0000

    [FIX] add missing image

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ const xlsx = require('node-xlsx').default;
 const pinataSDK = require('@pinata/sdk');
 const pinata = pinataSDK(process.env.API_KEY, process.env.API_SEC);
 
+const pinJsonToIPFS = async(body, options) => {
+  var result = await pinata.pinJSONToIPFS(body, options);
+  return result;
+}
+
 const pinFileToIPFS = async(filename) => {
   try {
-    if (fs.existsSync('./'+process.env.PNG_DIR+'/'+filename+'.png')) {
-      const readableStreamForFile = fs.createReadStream('./'+process.env.PNG_DIR+'/'+filename+'.png');
+    if (fs.existsSync('./'+process.env.PNG_DIR+'/'+filename+'.jpg')) {
+      const readableStreamForFile = fs.createReadStream('./'+process.env.PNG_DIR+'/'+filename+'.jpg');
       var result = await pinata.pinFileToIPFS(readableStreamForFile, {})
       return result
     }
@@ -95,12 +100,92 @@ const convertXlsFile = async () => {
   }
 }
 
+const uploadMissing = async () => {
+  var workSheetsFromIFile = [];
+  var iData = [];
+  var oData = [];
+
+  try {
+    if (fs.existsSync("missed.xlsx")) {
+      workSheetsFromIFile = xlsx.parse("missed.xlsx");
+      if(workSheetsFromIFile[0] && workSheetsFromIFile[0]['data']) {
+        iData = workSheetsFromIFile[0]['data']
+      }
+    }
+
+    // =====================================================
+    var index = 0;
+
+    while(iData[index] && iData[index][0] !== null && iData[index][0] !== undefined) {
+      var ret_pinata = await pinFileToIPFS(iData[index][0]);
+      oData[index] = iData[index];
+      oData[index][1] = ret_pinata.IpfsHash;
+      console.log(index);
+      index ++;
+    }
+
+    var buffer = xlsx.build([{name: "Sheet 1", data: oData}]);
+    fs.writeFile(process.env.OFILE, buffer,  "binary",function(err) { });
+    // =====================================================
+
+
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+
+const uploadMetadata = async () => {
+  const url_col = process.env.URL_COL;
+  var workSheetsFromIFile = [];
+
+  try {
+    if (fs.existsSync(process.env.oFILE)) {
+      workSheetsFromIFile = xlsx.parse(process.env.oFILE);
+      if(workSheetsFromIFile[0] && workSheetsFromIFile[0]['data']) {
+        iData = workSheetsFromIFile[0]['data']
+      }
+    }
+
+    // =====================================================
+    var index = 1;
+
+    while(iData[index] && iData[index][0] !== null && iData[index][0] !== undefined) {
+      var tmp = [];
+      for (var col=1; col<url_col; col++) {
+        tmp.push({
+          'attribute' : iData[0][col].toString().replace(/\s/g, ''),
+          'value' : iData[index][col]
+        });
+      }
+      var metadata = {
+        'name' : 'ExoWorld',
+        'tokenId' : iData[index][0],
+        'description' : 'This is belong to ExoWorld Planet',
+        'image' : 'https://gateway.pinata.cloud/ipfs/'+ iData[index][url_col],
+        'attributes' : tmp
+      }
+      var ret = await pinJsonToIPFS(metadata, {});
+      console.log(index, ret.IpfsHash);
+      index ++;
+    }
+    // =====================================================
+
+
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+
 const test = async() => {
   // const tmp = await pinFileToIPFS(0);
   // console.log(tmp)
   // tmp.IpfsHash
 
-  await convertXlsFile()
+  // await convertXlsFile();
+  await uploadMissing();
+  // await uploadMetadata();
 }
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
